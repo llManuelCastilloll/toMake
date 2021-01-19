7883fe796c3a08b9f07a4a7179c49d94f80628bc
Manuel Castillo <douglasskick@gmail.com>
2021-01-19 17:39:52 -0600

    last ll

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/Home';
 
-console.log("enter...");
+console.log("enter asa...");
 
 ReactDOM.render(<App />, document.getElementById("app"))
